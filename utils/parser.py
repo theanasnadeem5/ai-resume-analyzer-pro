fc4ce413343972a5ba4a29ed9feb13cf2f830112
anasbuilds <anasnadeem24career@gmail.com>
2026-07-03 02:35:54 +0530

    Create parser.py

---
+++ utils/parser.py
@@ -0,0 +1,12 @@
+import PyPDF2
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+
+        for page in reader.pages:
+            text += page.extract_text()
+
+    return text
